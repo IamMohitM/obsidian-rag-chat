33e4c6dfe3090e0d66d3f7946727fc6bb4100695
IamMohitM <mohitmotwani54@gmail.com>
2024-09-08 14:08:17 +0100

    auto update index and store index at exit

--- monitoring.py
+++ monitoring.py
@@ -5,12 +5,16 @@ from watchdog.events import FileSystemEventHandler
 import fnmatch
 from collections import deque
 
+
+# TODO: It's best for changes to be processed in batches
+# TODO: If same file goes through different operations, then the operations handling should be better.
+# for example if file is updated, there shouldn't be multiple entries for the update. just one update
 class FileChangeTracker:
     def __init__(self):
         self.changes = deque()
         self.file_status = {}
 
-    def add_change(self, event_type, file_path):
+    def add_change(self, event_type, file_path, new_path=None):
         self.changes.append((event_type, file_path))
         self.file_status[file_path] = event_type
 
@@ -21,29 +25,34 @@ class FileChangeTracker:
         self.changes.clear()
         self.file_status.clear()
 
+
 class MyHandler(FileSystemEventHandler):
     def __init__(self, tracker, ignore_patterns=None):
         self.tracker = tracker
         self.ignore_patterns = ignore_patterns or []
 
     def should_ignore(self, path):
-        return any(fnmatch.fnmatch(os.path.basename(path), pattern) for pattern in self.ignore_patterns)
+        return any(
+            fnmatch.fnmatch(os.path.basename(path), pattern)
+            for pattern in self.ignore_patterns
+        )
 
     def on_modified(self, event):
         if not event.is_directory and not self.should_ignore(event.src_path):
-            self.tracker.add_change('modified', event.src_path)
+            self.tracker.add_change("modified", event.src_path)
 
     def on_created(self, event):
         if not event.is_directory and not self.should_ignore(event.src_path):
-            self.tracker.add_change('created', event.src_path)
+            print(f"File created: {event.src_path}")
 
     def on_deleted(self, event):
         if not event.is_directory and not self.should_ignore(event.src_path):
-            self.tracker.add_change('deleted', event.src_path)
+            self.tracker.add_change("deleted", event.src_path)
 
     def on_moved(self, event):
         if not event.is_directory and not self.should_ignore(event.src_path):
-            self.tracker.add_change('moved', (event.src_path, event.dest_path))
+            self.tracker.add_change("moved", (event.src_path, event.dest_path))
+
 
 def watch_directory(path, ignore_patterns, interval=10):
     event_handler = MyHandler(ignore_patterns=ignore_patterns)
@@ -57,10 +66,10 @@ def watch_directory(path, ignore_patterns, interval=10):
         observer.stop()
     observer.join()
 
-    
-    
+
 if __name__ == "__main__":
     ignore_patterns = [".obsidian/*"]
-    path = os.path.join("/Users/mo/Library/Mobile Documents/iCloud~md~obsidian/Documents/MainVault")
+    path = os.path.join(
+        "/Users/mo/Library/Mobile Documents/iCloud~md~obsidian/Documents/MainVault"
+    )
     watch_directory(path, ignore_patterns)
-    
--- vd_update.py
+++ vd_update.py
@@ -2,13 +2,20 @@ import time
 import os
 import logging
 import sys
+import atexit
+import signal
 
-import lancedb
 from watchdog.observers import Observer
 from llama_index.core import VectorStoreIndex, StorageContext, Settings
-from llama_index.vector_stores.lancedb import LanceDBVectorStore
 from llama_index.readers.obsidian import ObsidianReader
 from llama_index.readers.file import MarkdownReader
+
+from llama_index.core import load_index_from_storage
+from llama_index.core.storage.docstore import SimpleDocumentStore
+from llama_index.core.vector_stores import SimpleVectorStore
+from llama_index.core.storage.index_store import SimpleIndexStore
+
+    
 # from llama_index.vect
 
 
@@ -23,6 +30,9 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 load_dotenv()
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 def update_llama_index_settings():
     api_key = os.environ.get("AZURE_OPENAI_API_KEY")
@@ -54,45 +64,35 @@ def update_llama_index_settings():
 
 # @ this should be a thread
 class VectorDatabaseUpdater:
-    def __init__(self, obsidian_path, uri, table_name="vectors"):
+    def __init__(self, obsidian_path, persist_dir):
         self.obsidian_path = obsidian_path
-        self.uri = uri
-        self.table_name = table_name
         self.tracker = FileChangeTracker()
-        self.vector_store = None
         self.index = None
-        self._initialize_vector_store()
-
-    def _initialize_vector_store(self):
-        #TODO: update this - load from LanceDB persistent storage
-        self._init_db_table(self.uri, self.table_name)
+        self.persist_dir = persist_dir
+        self._init_index()
+        atexit.register(self.save_index)
+        self._init_chat_engine()
+               
+    def _init_index(self):
         
-        if hasattr(self, 'lance_table') and self._init_db_table is not None:
-            self._index_from_lancedb(self.lance_table)
-        
-    def _init_db_table(self, path, table_name):
-        try:
-            self.db = lancedb.connect(uri=path)
-            self.lance_table = self.db.open_table(table_name)
-        except Exception as e:
-            print(e)
-        
-    def _index_from_lancedb(self, table):
-        self.vector_store = LanceDBVectorStore.from_table(table)
-        self.index = VectorStoreIndex.from_vector_store(self.vector_store)
-    
-    
-    def _compute_vector_store(self, path):
-        obsidian_documents = ObsidianReader(input_dir=self.path).load_data()
-        self.vector_store = LanceDBVectorStore(
-            uri=self.uri, mode="overwrite", query_type="hybrid"
+        storage_context = StorageContext.from_defaults(
+            docstore=SimpleDocumentStore.from_persist_dir(persist_dir=self.persist_dir),
+            vector_store=SimpleVectorStore.from_persist_dir(
+                persist_dir=self.persist_dir
+            ),
+            index_store=SimpleIndexStore.from_persist_dir(persist_dir=self.persist_dir),
         )
-        storage_context = StorageContext.from_defaults(vector_store=self.vector_store)
-        self.index = VectorStoreIndex.from_documents(
-            obsidian_documents, storage_context=storage_context
-        )
+        
+        self.index = load_index_from_storage(storage_context)
+        
+    def _init_chat_engine(self):
+        self.chat_engine = self.index.as_chat_engine()
+        
+    def chat(self, prompt):
+        streaming_response = self.chat_engine.stream_chat(prompt)
+        return "".join(streaming_response.response_gen)
 
-    def watch_directory(self, path, ignore_patterns, interval=10, compute_index=False):
+    def watch_directory(self, path, ignore_patterns, interval=5, compute_index=False):
         if compute_index:
             self._compute_vector_store(path)
             
@@ -107,6 +107,12 @@ class VectorDatabaseUpdater:
         except KeyboardInterrupt:
             observer.stop()
         observer.join()
+        
+    def save_index(self, persist_dir = None):
+        print("Saving Index")
+        persist_dir = persist_dir or self.persist_dir
+        self.index.storage_context.persist(persist_dir)
+        
 
     def process_changes(self):
         changes = self.tracker.get_changes()
@@ -120,23 +126,43 @@ class VectorDatabaseUpdater:
             elif event_type == 'moved':
                 self.move_file(file_path[0], file_path[1])
         self.tracker.clear_changes()
+        
+    # def load_file = 
+        
+    def read_file(self, file_path):
+        document = MarkdownReader().load_data(file_path)
+            
+        metadata = {
+            "file_path": file_path,
+            "filename": os.path.basename(file_path),
+        }
+        
+        for chunk in document:
+            chunk.metadata = metadata
+            chunk.doc_id = file_path
+        
+        return document
 
     def add_file(self, file_path):
         print(f"Add file operations {file_path}")
-        document = MarkdownReader().load_data(file_path)
-        
-        self.index.insert(document[0])
+        try:
+            document = self.read_file(file_path)
+            
+            for chunk in document:
+                self.index.insert(chunk)
+                
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
 
     def update_file(self, file_path):
         print(f"Update file operations{file_path}")
-        # Option 1: Update existing file
-        self.delete_file(file_path)
-        self.add_file(file_path)
+        
+        documents = self.read_file(file_path)
+        self.index.refresh_ref_docs(documents)
 
     def delete_file(self, file_path):
         print(f"Delete file operations{file_path}")
-        # Assuming the vector store has a method to delete by document ID or file path
-        self.vector_store.delete(file_path)
+        self.index.delete_ref_doc(file_path, delete_from_docstore=True)
 
     def move_file(self, src_path, dest_path):
         print(f"Move file operations{src_path} -> {dest_path}")
@@ -150,14 +176,14 @@ class VectorDatabaseUpdater:
         
 if __name__ == "__main__":
     ignore_patterns = [".obsidian/*"]
-    path = os.path.join("/Users/mo/Library/Mobile Documents/iCloud~md~obsidian/Documents/MainVault")
-    
+    path = os.path.join("/Users/mo/Library/Mobile Documents/iCloud~md~obsidian/Documents/MainVault/TechNotes")
+    persist_dir = "./data/tech_notes"
     uri = "./data/obsidian_lancdb"
     table_name = 'vectors'
     
     update_llama_index_settings()
     
-    updater = VectorDatabaseUpdater(obsidian_path=path, uri=uri, table_name=table_name)
+    updater = VectorDatabaseUpdater(obsidian_path=path, persist_dir=persist_dir)
     
     
     updater.watch_directory(path = path, ignore_patterns=ignore_patterns)
